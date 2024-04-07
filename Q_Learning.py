b3bfa485436d2096171ef70364aab10bdef1b623
JungHoon Kim <kik1232198@gmail.com>
2024-04-07 11:19:04 +0900

    first commit

---
+++ Q_Learning.py
@@ -0,0 +1,27 @@
+import gymnasium as gym
+# env = gym.make("ALE/AirRaid-v5", render_mode="human")
+env = gym.make("FetchPickAndPlace-v2", render_mode="human")
+
+observation, info = env.reset(seed=42)
+total_reward = 0
+last_reward = 0
+for _ in range(1000):
+
+   if last_reward >0:
+      action = last_action
+   else:
+      action = env.action_space.sample()
+
+   observation, reward, terminated, truncated, info = env.step(action)
+
+   total_reward += last_reward
+   last_reward = reward
+   last_action = action
+
+   if terminated or truncated:
+      observation, info = env.reset()
+      total_reward = 0
+      last_reward = 0
+env.close()
+
+print(f"총 보상: {total_reward}")
