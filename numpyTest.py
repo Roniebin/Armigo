738fb89d2ccc15b57205e7f9918b4a4dbd2f7a5a
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-06-11 06:36:39 +0900

    TQC2

---
+++ numpyTest.py
@@ -0,0 +1,23 @@
+import numpy as np
+a = [4.2, 5.2, 4.4, 5.4, 1.5, 3.8, 45.4, 25.8, 6.9]
+b = [0.2, -0.2, 0.01, 0.2, 0, 0.01, 0.2, 0.2, 0.01]
+
+a_ndarray = np.array(a, np.float32)
+b_ndarray = np.array(b, np.float32)
+
+
+def distance(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    assert a.shape == b.shape
+    return np.linalg.norm(a - b, axis=-1)
+
+
+num_objects = 3
+rt = 0
+for i in range(num_objects):
+    d = distance(a_ndarray[i*3:(i+1)*3], b_ndarray[i*3:(i+1)*3])
+    rt += -np.log(d + 1)
+    print(rt)
+
+d = distance(a_ndarray, b_ndarray)
+log_reward = -np.log(d + 1)
+print(log_reward)
